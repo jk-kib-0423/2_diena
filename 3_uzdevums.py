087e8bb69372f5be0f48790bb50d3ed23e65ba11
vabulsb <vabulsb@inbox.lv>
2023-05-29 19:04:05 +0300

    izmainas

---
+++ 3_uzdevums.py
@@ -0,0 +1,6 @@
+skaitlis = int(input("Lūdzu, ievadiet skaitli: "))
+
+if skaitlis % 2 == 0:
+    print("Ievadītais skaitlis ir pāra skaitlis.")
+else:
+    print("Ievadītais skaitlis ir nepāra skaitlis.")
