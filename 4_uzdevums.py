3282ae0ca7b5fc4610762ae2baf3d75371e9d8c3
vabulsb <vabulsb@inbox.lv>
2023-06-01 22:18:34 +0300

    izmaiņas

--- 4_uzdevums.py
+++ 4_uzdevums.py
@@ -1,10 +1,22 @@
-vērtējums = int(input("Lūdzu, ievadiet savu vērtējumu programmēšanā (no 1 līdz 10): "))
+vērtējums = int(input("Lūdzu, ievadiet savu vērtējumu: "))
 
 if vērtējums == 10:
-    print("Izcili!")
-elif vērtējums >= 8:
-    print("Teicami!")
-elif vērtējums >= 5:
-    print("Viduvēji!")
-else:
-    print("Vāji!")
+    print("Izcili")
+elif vērtējums == 9:
+    print("Teicami")
+elif vērtējums == 8:
+    print("Ļoti labi")
+elif vērtējums == 7:
+    print("Labī")
+elif vērtējums == 6:
+    print("Gandrīz labi")
+elif vērtējums == 5:
+    print("Viduvēji")
+elif vērtējums == 4:
+    print("Gandrīz viduvēji")
+elif vērtējums == 3:
+    print("Vāji")
+elif vērtējums == 2:
+    print("Ļoti vāji")
+elif vērtējums == 1:
+    print("Ļoti, ļoti vāji")
